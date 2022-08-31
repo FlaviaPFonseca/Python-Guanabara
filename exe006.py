33b7f1ab83330d7b4152cd75b231e2e692911176
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-08-30 23:48:11 -0300

    Add files via upload
    exercícios 6 de aritmética em python

---
+++ exe006.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um numero e
+# mostre seu dobro, triplo e raiz quadrada.
+n = int(input('Digite um número:'))
+a = n * 2
+b = n * 3
+c = n ** (1/2)
+print('o dobro de {} vale:{} \n o triplo vale:{} e \n a raiz quadrada vale:{}'.format(n, a, b, c))
