18a25c176876f5caebf04a2e2a5b4f2b6a2f4e28
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-08-30 22:56:43 -0300

    Add files via upload

---
+++ exe004.py
@@ -0,0 +1,7 @@
+a = input('digite algo:')
+print('O tipo primitivo desse algo é',type(a))
+print('So tem espaços?', a.isspace())
+print('É número?',a.isnumeric())
+print('É alfabetico?',a.isalpha())
+
+
