003c5d24f512f0bc70b0eb162ee83695d00d2d72
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-08-30 23:54:28 -0300

    Add files via upload
    ordem de precedência, aritmética em Python

---
+++ exe007.py
@@ -0,0 +1,7 @@
+# desenvolva um programa que leia ás
+# duas notas de um aluno e calcule a sua média
+n1 = float(input('primeira nota do aluno:'))
+n2 = float(input('segunda nota do aluno:'))
+m = (n1 + n2) / 2
+print(' A media entre {} e {} é igual a {}'.format(n1, n2,m))
+
